f1b72b2bdf3218f67d167ad17deba1b0438a27f5
Hadoop <Hadoop@Bos-MacBook-Pro.local>
2016-02-04 10:02:31 +0800

    LinkedIn Sign In Negative Test(v 2.0)
    Finish all negative path testing based on the instruction.
    
    Error Message are included in Constants.

--- Code/LinkedInSignInNegativeTest.java
+++ Code/LinkedInSignInNegativeTest.java
@@ -1,5 +1,6 @@
 /**
 * Created by Bo Man on 2/3/16.
+* Latest Revised by Bo Man on 2/4/16.
 * This test is used to verify that
 *      LinkedIn can not be signed in correctly once the user gives error inputs and
 *      error message will be correspondingly shown to user reminding him/her to try again.
@@ -90,7 +91,166 @@ public class LinkedInSignInNegativeTest {
 
    }
 
-   // -- PATH (22)
+   /******************************* NEGATIVE TESTING THREE ******************************/
+   // Unsuccessfully SignIn with error message
+   // Negative path (illegal username and illegal password)
+   // path covered (1) (2) (3) (5) (7) (10) (15) (23) (24) (25)
+   // Conditional Combination B + Q
+   @Test
+   public void linkedInSingInNegativeTestThree (){
+       // 1. path covered (1) (2)(3) (5) (7) (10) (15)
+       LinkedInStaticMethod.linkedInSignIn(driver, response, Constants.illegalUsername, Constants.illegalPassword);
+       // 2. Get Sign In Failure Page -- PATH (23)
+       String response = driver.getPageSource();
+       // 3. Validate error info -- PATH (24)
+       // including error message, illegal username and illegal password
+       assertTrue("Validation FAILED - Text \"" + Constants.errorMessageInfoErrorMessage + "\" NOT FOUND",
+               response.contains(Constants.errorMessageInfo));
+       assertTrue("Validation FAILED - Text \"" + Constants.illegalUsernameErrorMessage + "\" NOT FOUND",
+               response.contains(Constants.illegalUsernameInfo));
+       assertTrue("Validation FAILED - Text \"" + Constants.illegalPasswordErrorMessage + "\" NOT FOUND",
+               response.contains(Constants.illegalPasswordInfo));
+   }
+
+   /******************************* NEGATIVE TESTING FOUR ******************************/
+   // Unsuccessfully SignIn with error message
+   // Negative path (invalid username and illegal password)
+   // path covered (1) (2) (3) (5) (7) (10) (15) (23) (24) (25)
+   // Conditional Combination D + Q
+   @Test
+   public void linkedInSingInNegativeTestFour (){
+       // 1. path covered (1) (2)(3) (5) (7) (10) (15)
+       LinkedInStaticMethod.linkedInSignIn(driver, response, Constants.invalidUsername, Constants.illegalPassword);
+       // 2. Get Sign In Failure Page -- PATH (23)
+       String response = driver.getPageSource();
+       // 3. Validate error info -- PATH (24)
+       // including error message, invalid username and illegal password
+       assertTrue("Validation FAILED - Text \"" + Constants.errorMessageInfoErrorMessage + "\" NOT FOUND",
+               response.contains(Constants.errorMessageInfo));
+       assertTrue("Validation FAILED - Text \"" + Constants.invalidUsernameErrorMessage + "\" NOT FOUND",
+               response.contains(Constants.invalidUsernameInfo));
+       assertTrue("Validation FAILED - Text \"" + Constants.illegalPasswordErrorMessage + "\" NOT FOUND",
+               response.contains(Constants.illegalPasswordInfo));
+   }
+
+   /******************************* NEGATIVE TESTING FIVE ******************************/
+   // Unsuccessfully SignIn with error message
+   // Negative path (illegal username but legal password)
+   // path covered (1) (2) (3) (5) (7) (10) (15) (23) (24) (25)
+   // Conditional Combination B + R
+   @Test
+   public void linkedInSingInNegativeTestFive (){
+       // 1. path covered (1) (2)(3) (5) (7) (10) (15)
+       LinkedInStaticMethod.linkedInSignIn(driver, response, Constants.illegalUsername, Constants.legalPassword);
+       // 2. Get Sign In Failure Page -- PATH (23)
+       String response = driver.getPageSource();
+       // 3. Validate error info -- PATH (24)
+       // including error message, invalid username and illegal password
+       assertTrue("Validation FAILED - Text \"" + Constants.errorMessageInfoErrorMessage + "\" NOT FOUND",
+               response.contains(Constants.errorMessageInfo));
+       assertTrue("Validation FAILED - Text \"" + Constants.illegalUsernameErrorMessage + "\" NOT FOUND",
+               response.contains(Constants.illegalUsernameInfo));
+   }
+
+   /******************************* NEGATIVE TESTING SIX ******************************/
+   // Unsuccessfully SignIn with error message
+   // Negative path (invalid username but legal password)
+   // path covered (1) (2) (3) (5) (7) (10) (15) (23) (24) (25)
+   // Conditional Combination D + R
+   @Test
+   public void linkedInSingInNegativeTestSix (){
+       // 1. path covered (1) (2)(3) (5) (7) (10) (15)
+       LinkedInStaticMethod.linkedInSignIn(driver, response, Constants.invalidUsername, Constants.legalPassword);
+       // 2. Get Sign In Failure Page -- PATH (23)
+       String response = driver.getPageSource();
+       // 3. Validate error info -- PATH (24)
+       // including error message, invalid username and illegal password
+       assertTrue("Validation FAILED - Text \"" + Constants.errorMessageInfoErrorMessage + "\" NOT FOUND",
+               response.contains(Constants.errorMessageInfo));
+       assertTrue("Validation FAILED - Text \"" + Constants.invalidUsernameErrorMessage + "\" NOT FOUND",
+               response.contains(Constants.invalidUsernameInfo));
+   }
+
+   /******************************* NEGATIVE TESTING SEVEN ******************************/
+   // Unsuccessfully SignIn with error message
+   // Negative path (incorrect username and illegal password)
+   // ONLY RETURN ILLEGAL PASSWORD ERROR MESSAGE (SINCE PRIORITY: ILLEGAL > INCORRECT)
+   // path covered (1) (2) (3) (5) (7) (10) (15) (23) (24) (25)
+   // Conditional Combination F + Q
+   @Test
+   public void linkedInSingInNegativeTestSeven () {
+       // 1. path covered (1) (2)(3) (5) (7) (10) (15)
+       LinkedInStaticMethod.linkedInSignIn(driver, response, Constants.incorrectUsername, Constants.illegalPassword);
+       // 2. Get Sign In Failure Page -- PATH (23)
+       String response = driver.getPageSource();
+       // 3. Validate error info -- PATH (24)
+       // including error message, invalid username and illegal password
+       assertTrue("Validation FAILED - Text \"" + Constants.errorMessageInfoErrorMessage + "\" NOT FOUND",
+               response.contains(Constants.errorMessageInfo));
+       assertTrue("Validation FAILED - Text \"" + Constants.illegalPasswordErrorMessage + "\" NOT FOUND",
+               response.contains(Constants.illegalPasswordInfo));
+   }
+
+   /******************************* NEGATIVE TESTING EIGHT ******************************/
+   // Unsuccessfully SignIn with error message
+   // Negative path (correct username but illegal password)
+   // ONLY RETURN ILLEGAL PASSWORD ERROR MESSAGE (SINCE PRIORITY: ILLEGAL > CORRECT)
+   // path covered (1) (2) (3) (5) (7) (10) (15) (23) (24) (25)
+   // Conditional Combination G + Q
+   @Test
+   public void linkedInSingInNegativeTestEight () {
+       // 1. path covered (1) (2)(3) (5) (7) (10) (15)
+       LinkedInStaticMethod.linkedInSignIn(driver, response, Constants.username, Constants.illegalPassword);
+       // 2. Get Sign In Failure Page -- PATH (23)
+       String response = driver.getPageSource();
+       // 3. Validate error info -- PATH (24)
+       // including error message, invalid username and illegal password
+       assertTrue("Validation FAILED - Text \"" + Constants.errorMessageInfoErrorMessage + "\" NOT FOUND",
+               response.contains(Constants.errorMessageInfo));
+       assertTrue("Validation FAILED - Text \"" + Constants.illegalPasswordErrorMessage + "\" NOT FOUND",
+               response.contains(Constants.illegalPasswordInfo));
+   }
+
+   /******************************* NEGATIVE TESTING NINE ******************************/
+   // Unsuccessfully SignIn with error message
+   // Negative path (incorrect username but legal password)
+   // ONLY RETURN INCORRECT USERNAME ERROR MESSAGE
+   // path covered (1) (2) (3) (5) (7) (10) (15) (23) (24) (25)
+   // Conditional Combination F + R
+   @Test
+   public void linkedInSingInNegativeTestNine () {
+       // 1. path covered (1) (2)(3) (5) (7) (10) (15)
+       LinkedInStaticMethod.linkedInSignIn(driver, response, Constants.incorrectUsername, Constants.legalPassword);
+       // 2. Get Sign In Failure Page -- PATH (23)
+       String response = driver.getPageSource();
+       // 3. Validate error info -- PATH (24)
+       // including error message, invalid username and illegal password
+       assertTrue("Validation FAILED - Text \"" + Constants.errorMessageInfoErrorMessage + "\" NOT FOUND",
+               response.contains(Constants.errorMessageInfo));
+       assertTrue("Validation FAILED - Text \"" + Constants.incorrectUsernameErrorMessage + "\" NOT FOUND",
+               response.contains(Constants.incorrectUsernameInfo));
+   }
+
+   /******************************* NEGATIVE TESTING TEN ******************************/
+   // Unsuccessfully SignIn with error message
+   // Negative path (correct username but incorrect password)
+   // path covered (1) (2) (3) (5) (7) (10) (15) (23) (24) (25)
+   // Conditional Combination G + S
+   @Test
+   public void linkedInSingInNegativeTestTen () {
+       // 1. path covered (1) (2)(3) (5) (7) (10) (15)
+       LinkedInStaticMethod.linkedInSignIn(driver, response, Constants.username, Constants.incorrectPassword);
+       // 2. Get Sign In Failure Page -- PATH (23)
+       String response = driver.getPageSource();
+       // 3. Validate error info -- PATH (24)
+       // including error message, invalid username and illegal password
+       assertTrue("Validation FAILED - Text \"" + Constants.errorMessageInfoErrorMessage + "\" NOT FOUND",
+               response.contains(Constants.errorMessageInfo));
+       assertTrue("Validation FAILED - Text \"" + Constants.incorrectPasswordErrorMessage + "\" NOT FOUND",
+               response.contains(Constants.incorrectPasswordInfo));
+   }
+
+   // -- PATH (22) & (25)
    @After
    public void tearDown(){
        driver.close();
